a8c86e426a46342e2c79cf0d0052fd0abf1c9140
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-04-01 15:13:01 +0200

    "hotfix"

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,8 +4,10 @@ from api.v1.views import app_views
 from flask import Flask
 from models import storage
 
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,9 +4,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status')
+@app_views.route('/status', strict_slashes=False)
 def status():
     '''return stuff'''
-    test = {"status": "OK"}
-    test = jsonify(test)
-    return test
+    return jsonify(status='OK')
