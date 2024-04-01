c126d2a99c5b7d3bb7cdf4bf8d7f830d4438e2d0
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-04-01 15:16:45 +0200

    "hotfix"

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,25 @@
 '''index file'''
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     '''return stuff'''
     return jsonify(status='OK')
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    '''return stats'''
+    classes = {"Amenity": "amenities",
+               "City": "cities",
+               "Place": "places",
+               "Review": "reviews",
+               "State": "states",
+               "User": "users"}
+    new_dict = {}
+    for key, value in classes.items():
+        new_dict[value] = storage.count(key)
+    return jsonify(new_dict)
