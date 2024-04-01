a42bc70b4e3bda77bc5f45ad3fe9ad0201bea9d8
youssef2272002salah <youssef22772002salah@gmail.com>
2024-04-01 21:14:47 +0200

    1

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
-'''application file'''
-from api.v1.views import app_views
-from flask import Flask, jsonify
+"""app.py to connect to API"""
+import os
 from models import storage
+from api.v1.views import app_views
+from flask import Flask, Blueprint, jsonify, make_response
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown_app(self):
-    '''Status of your API'''
+def teardown_appcontext(code):
+    """teardown_appcontext"""
     storage.close()
 
 
 @app.errorhandler(404)
 def page_not_found(error):
-    '''404 error'''
-    return jsonify({'error': 'Not found'}), 404
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000', threaded=True)
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', '5000')))
