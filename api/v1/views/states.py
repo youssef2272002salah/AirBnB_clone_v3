a56b4ef7e6dcd451049850ad53679dbcdcc35043
youssef2272002salah <youssef22772002salah@gmail.com>
2024-04-02 02:00:40 +0200

    4

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,66 +1,66 @@
 #!/usr/bin/python3
-"""states.py"""
-
+'''State file'''
 from api.v1.views import app_views
-from flask import abort, jsonify, make_response, request
+from flask import jsonify, request
 from models import storage
 from models.state import State
 
 
-@app_views.route('/states', methods=['GET'], strict_slashes=False)
-def get_states():
-    """get state information for all states"""
-    states = []
-    for state in storage.all("State").values():
-        states.append(state.to_dict())
-    return jsonify(states)
+@app_views.route('/states', strict_slashes=False)
+def all_states():
+    all_data = storage.all(State)
+    new_list = []
+    for state in all_data.values():
+        new_list.append(state.to_dict())
+    return jsonify(new_list)
 
 
-@app_views.route('/states/<string:state_id>', methods=['GET'],
-                 strict_slashes=False)
-def get_state(state_id):
-    """get state information for specified state"""
-    state = storage.get("State", state_id)
-    if state is None:
-        abort(404)
-    return jsonify(state.to_dict())
+@app_views.route('/states/<string:state_id>', strict_slashes=False)
+def one_state(state_id):
+    the_state = storage.get(State, state_id)
+    if the_state is None:
+        return jsonify({'error': 'Not found'}), 404
+    the_state = the_state.to_dict()
+    return jsonify(the_state)
 
 
-@app_views.route('/states/<string:state_id>', methods=['DELETE'],
+@app_views.route('/states/<string:state_id>',
+                 methods=['DELETE'],
                  strict_slashes=False)
 def delete_state(state_id):
-    """deletes a state based on its state_id"""
-    state = storage.get("State", state_id)
-    if state is None:
-        abort(404)
-    state.delete()
+    the_state = storage.get(State, state_id)
+    if the_state is None:
+        return jsonify({'error': 'Not found'}), 404
+    storage.delete(the_state)
     storage.save()
-    return (jsonify({}))
+    return jsonify({}), 200
 
 
-@app_views.route('/states/', methods=['POST'], strict_slashes=False)
-def post_state():
-    """create a new state"""
-    if not request.get_json():
-        return make_response(jsonify({'error': 'Not a JSON'}), 400)
-    if 'name' not in request.get_json():
-        return make_response(jsonify({'error': 'Missing name'}), 400)
-    state = State(**request.get_json())
+@app_views.route('/states', methods={'POST'}, strict_slashes=False)
+def create_state():
+    new_state = request.get_json()
+    if new_state is None:
+        return jsonify({'error': 'Not a JSON'}), 400
+    if 'name' not in new_state:
+        return jsonify({'error': 'Missing name'}), 400
+    state = State(**new_state)
     state.save()
-    return make_response(jsonify(state.to_dict()), 201)
+    storage.save()
+    return jsonify(state.to_dict()), 201
 
 
-@app_views.route('/states/<string:state_id>', methods=['PUT'],
-                 strict_slashes=False)
-def put_state(state_id):
-    """update a state"""
-    state = storage.get("State", state_id)
-    if state is None:
-        abort(404)
-    if not request.get_json():
-        return make_response(jsonify({'error': 'Not a JSON'}), 400)
-    for attr, val in request.get_json().items():
-        if attr not in ['id', 'created_at', 'updated_at']:
-            setattr(state, attr, val)
-    state.save()
-    return jsonify(state.to_dict())
+@app_views.route('/states/<string:state_id>',
+                 methods={'PUT'}, strict_slashes=False)
+def edit_state(state_id):
+    new_state = request.get_json()
+    the_state = storage.get(State, state_id)
+    if new_state is None:
+        return jsonify({'error': 'Not a JSON'}), 400
+    if the_state is None:
+        return jsonify({'error': 'Not found'}), 404
+    for key, value in new_state.items():
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(the_state, key, value)
+    storage.save()
+    return jsonify(the_state.to_dict()), 200
+
